af35f7fdce7bee6dfca9c2035511e5a8bbc7aa2e
mochamadgalih <mochamadglh@gmail.com>
2024-11-09 17:42:42 +0700

    add to swagger

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -11,32 +11,53 @@ import {
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('books')
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new book' })
+  @ApiResponse({
+    status: 200,
+    description: 'Book created',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid input data.' })
   create(@Body() createBookDto: CreateBookDto) {
     return this.bookService.create(createBookDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get All Books' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return All Books',
+  })
   findAll() {
     return this.bookService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Return a book.' })
+  @ApiResponse({ status: 404, description: 'Book not found.' })
   findOne(@Param('id') id: string) {
     return this.bookService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a book by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Book updated',
+  })
   update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
     return this.bookService.update(+id, updateBookDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a book by ID (soft delete)' })
   remove(@Param('id') id: string) {
     return this.bookService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,17 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookshelf API')
+    .setDescription('BookShelf Description')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
